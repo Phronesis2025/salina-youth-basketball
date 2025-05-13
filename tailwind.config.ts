545c9b833e345143f1b949c07586f75f7528f4cd
Jason Boyer <phronesis700@gmail.com>
2025-05-13 13:57:16 -0500

    Fix button color with inline Tailwind classes, update tailwind.config.ts for custom colors, complete initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#1C2526", // Navy
-        accent: "#D91E18", // Red
-        text: "#FFFFFF", // White
+        primary: "#1C2526",
+        accent: "#D91E18",
+        text: "#FFFFFF",
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
